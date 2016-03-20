45057348e549a8a0ed02c6acbbe94dda92ff14d7
SteveMcArthur <contact@stevemcarthur.co.uk>
2016-03-20 11:04:09 +0000

    try catch

--- index.js
+++ index.js
@@ -9,35 +9,41 @@ var startServer = require("./lib/start-server");
 
 var HTTP_OK = 200;
 var ip;
-var pt;
+var port;
 var siteUrl;
+var options;
 
 function initialiseTests(server) {
 	request.get(siteUrl).end(function (error, res) {
 		assert.equal(res.statusCode, HTTP_OK, 'status code');
-		pagesEx.pagesExist(siteUrl, server);
+		pagesEx.pagesExist(options, server);
 	});
 }
 
-function runTests(host, port) {
-
-	ip = host || '127.0.0.1';
-	pt = port || '9778';
-	siteUrl = "http://" + ip + ":" + pt;
-	request.get(siteUrl).end(function (error, res) {
-		if (res && res.statusCode === HTTP_OK) {
-			console.log("Server is already running on " + siteUrl);
-			//if the server is already running then we have no need
-			//to pass a server object to initializeTests as we don't
-			//need to shut it down
-			initialiseTests();
-		} else {
-			startServer.start({
-				ip: ip,
-				port: pt
-			}, initialiseTests)
-		}
-	});
+function runTests(opts) {
+	try {
+		options = opts || {};
+		ip = options.host || '127.0.0.1';
+		port = options.port || '9778';
+		siteUrl = "http://" + ip + ":" + port;
+		request.get(siteUrl).end(function (error, res) {
+			if (res && res.statusCode === HTTP_OK) {
+				console.log("Server is already running on " + siteUrl);
+				//if the server is already running then we have no need
+				//to pass a server object to initializeTests as we don't
+				//need to shut it down
+				initialiseTests();
+			} else {
+				startServer.start({
+					ip: ip,
+					port: port
+				}, initialiseTests)
+			}
+		});
+	} catch (err) {
+		console.log(err);
+		throw err;
+	}
 }
 
 module.exports.runTests = runTests;
